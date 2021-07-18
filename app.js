ceab3d5e1d87b3138412cb5c7cec027a1c9e0a75
Dhayor58 <olayinkatemidayo.m@gmail.com>
2021-07-18 17:45:41 +0100

    added links to nav-links

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const btn = document.querySelector('.toggle-btn');
 const container = document.querySelector('.nav-links')
+const connect = document.querySelector('.connect')
+
+// connect.addEventListener('hover', => {
+//     connect.classList.toggle('show-connect')
+// })
 
 btn.addEventListener('click', function () {
     btn.classList.toggle('show-link');
     container.classList.toggle('show-nav-link')
-})
+});
+
